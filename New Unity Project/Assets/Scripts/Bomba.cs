e0001d9920342c46bc7378b21eaab176372a08e7
Kevin Chinchilla <a00825945@itesm.mx>
2018-11-26 23:17:27 -0600

    chain reaction

--- New Unity Project/Assets/Scripts/Bomba.cs
+++ New Unity Project/Assets/Scripts/Bomba.cs
@@ -6,28 +6,85 @@ using System;
 public class Bomba : MonoBehaviour {
     public GameObject explosionPrefab;
     public LayerMask levelMask;
-	// Use this for initialization
-	void Start () {
+    public bool exploded = false;
+    // Use this for initialization
+    void Start() {
         Invoke("Explode", 3f);
-     
 
 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
 
     void Explode()
     {
-        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity); 
+
+        GetComponent<MeshRenderer>().enabled = false; 
+        exploded = true;
+        //transform.Find("Collider").gameObject.SetActive(false); 
+        Destroy(gameObject, .3f); 
+       
+        
 
 
-        GetComponent<MeshRenderer>().enabled = false;
-        transform.Find("Collider").gameObject.SetActive(false);
-        Destroy(gameObject, .3f);
+        StartCoroutine(CreateExplosions(Vector3.forward));
+        StartCoroutine(CreateExplosions(Vector3.right));
+        StartCoroutine(CreateExplosions(Vector3.back));
+        StartCoroutine(CreateExplosions(Vector3.left));
+        
+
 
     }
-  
+    public IEnumerator CreateExplosions(Vector3 direction)
+    {
+
+        for (int i = 1; i < 3; i++)
+        {
+
+            RaycastHit hit;
+
+            Physics.Raycast(transform.position + new Vector3(0, .5f, 0), direction, out hit,
+              i, levelMask);
+
+
+            if (!hit.collider)
+            {
+                Instantiate(explosionPrefab, transform.position + (i * direction),
+
+                  explosionPrefab.transform.rotation);
+
+            }
+            else
+            {
+                break;
+            }
+
+            yield return new WaitForSeconds(.05f);
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!exploded && other.CompareTag("Explosion"))
+        //if ( other.CompareTag("Explosion"))
+        {
+            CancelInvoke("Explode");
+            Explode();
+        }
+        if ( other.CompareTag( "Destructable"))
+        {
+            Destroy(other.gameObject);
+        }
+       // else if ( other.ComapreTag.())
+
+
+    }
+
+
 }
